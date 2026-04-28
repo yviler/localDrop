6308dfb0acf052ba2a52a8d7eb65661388d60c15
yviler <muhazzriel@gmail.com>
2026-04-28 15:03:11 +0700

    initialized item class, TODO: use in the browse route

--- utils/file.py
+++ utils/file.py
@@ -2,8 +2,15 @@ import os
 from app.models.models import Item
 
 def createItemObj(items:list,current_dir:str)-> list:
+    objList = []
     for item in items:
         full_item = os.path.join(current_dir,item)
         stats = os.stat(full_item)
-        print(stats)
-        break
+        objList.append(Item(
+            filename=item,
+            filesize=stats.st_size,
+            filetype=os.path.splitext(item)[1],
+            location=current_dir,
+            isDirectory=os.path.isdir(full_item)
+        ))
+    return objList
