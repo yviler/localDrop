ac81701e2e13ce366a5d398099cda7bd82e84056
yviler <muhazzriel@gmail.com>
2026-04-29 20:04:19 +0700

    helper to normalize file size

--- utils/file.py
+++ utils/file.py
@@ -8,9 +8,25 @@ def createItemObj(items:list,current_dir:str)-> list:
         stats = os.stat(full_item)
         objList.append(Item(
             filename=item,
-            filesize=stats.st_size,
+            filesize=normalizeFileSize(stats.st_size),
             filetype=os.path.splitext(item)[1],
             location=current_dir,
             isDirectory=os.path.isdir(full_item)
         ))
     return objList
+
+def normalizeFileSize(filesize:int)-> str:
+    count = 0
+    sizeDict = {
+        0: "Bytes",
+        1: "KB",
+        2: "MB",
+        3: "GB",
+        4: "TB",
+        5: "PB"
+    }
+    while(filesize>999):
+        filesize/=1000
+        count+=1
+    filesize = str(round(filesize,2)) + sizeDict.get(count)
+    return filesize
